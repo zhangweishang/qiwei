7d372689a2617a87ab497c554cf5d026d9ee733b
123 <1789139001@qq.com>
2019-08-10 12:25:37 +0800

    添加地址表单

--- pages/address/address.js
+++ pages/address/address.js
@@ -1,13 +1,77 @@
 // pages/address/address.js
+const tcity = require("../../utils/citys.js");
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    id: '',
+    provinces: [],
+    province: "",
+    citys: [],
+    city: "",
+    countys: [],
+    county: '',
+    value: [0, 0, 0],
+    values: [0, 0, 0],
+    condition: false
   },
+  bindChange: function (e) {
+    var val = e.detail.value
+    var t = this.data.values;
+    var cityData = this.data.cityData;
 
+    if (val[0] != t[0]) {
+      const citys = [];
+      const countys = [];
+
+      for (let i = 0; i < cityData[val[0]].sub.length; i++) {
+        citys.push(cityData[val[0]].sub[i].name)
+      }
+      for (let i = 0; i < cityData[val[0]].sub[0].sub.length; i++) {
+        countys.push(cityData[val[0]].sub[0].sub[i].name)
+      }
+
+      this.setData({
+        province: this.data.provinces[val[0]],
+        city: cityData[val[0]].sub[0].name,
+        citys: citys,
+        county: cityData[val[0]].sub[0].sub[0].name,
+        countys: countys,
+        values: val,
+        value: [val[0], 0, 0]
+      })
+      return;
+    }
+    if (val[1] != t[1]) {
+      const countys = [];
+
+      for (let i = 0; i < cityData[val[0]].sub[val[1]].sub.length; i++) {
+        countys.push(cityData[val[0]].sub[val[1]].sub[i].name)
+      }
+
+      this.setData({
+        city: this.data.citys[val[1]],
+        county: cityData[val[0]].sub[val[1]].sub[0].name,
+        countys: countys,
+        values: val,
+        value: [val[0], val[1], 0]
+      })
+      return;
+    }
+    if (val[2] != t[2]) {
+      this.setData({
+        county: this.data.countys[val[2]],
+        values: val
+      })
+      return;
+    }
+  },
+  open: function () {
+    this.setData({
+      condition: !this.data.condition
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -16,8 +80,40 @@ Page({
     that.setData({
       id: options.id
     })
-  },
+    tcity.init(that);
 
+    var cityData = that.data.cityData;
+    const provinces = [];
+    const citys = [];
+    const countys = [];
+
+    for (let i = 0; i < cityData.length; i++) {
+      provinces.push(cityData[i].name);
+    }
+    for (let i = 0; i < cityData[0].sub.length; i++) {
+      citys.push(cityData[0].sub[i].name)
+    }
+    for (let i = 0; i < cityData[0].sub[0].sub.length; i++) {
+      countys.push(cityData[0].sub[0].sub[i].name)
+    }
+
+    that.setData({
+      'provinces': provinces,
+      'citys': citys,
+      'countys': countys,
+      'province': cityData[0].name,
+      'city': cityData[0].sub[0].name,
+      'county': cityData[0].sub[0].sub[0].name
+    })
+  },
+  //提交表单触发
+  formSubmit: function (e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+  },
+  //删除地址
+  delete: function(e){
+    console.log('delete事件，携带数据为：', e.currentTarget.dataset.id)
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
