357a8fbdb05be3e1ba62a8eafe490950a96b66c0
123 <1789139001@qq.com>
2019-08-10 17:24:34 +0800

    商品列表

--- pages/index/index.js
+++ pages/index/index.js
@@ -16,12 +16,42 @@ Page({
     }, {
       "url": "../../images/wx_logo.png"
     }],
+    "list": [{
+      "id":"1",
+      "title": "炒粉",
+      "desc":"华软炒粉",
+      "month_sales":"48",
+      "like":"3",
+      "sell_price":"18",
+      "cover":"../../images/wx_logo.png",
+      "number": 0
+    },
+    {
+      "id": "2",
+      "title": "河粉",
+      "desc": "华软河粉",
+      "month_sales": "428",
+      "like": "32",
+      "sell_price": "148",
+      "cover": "../../images/wx_logo.png",
+      "number": 0
+      },
+      {
+        "id": "3",
+        "title": "面",
+        "desc": "华软面",
+        "month_sales": "4",
+        "like": "83",
+        "sell_price": "148",
+        "cover": "../../images/wx_logo.png",
+        "number": 0
+      }],
     indicatorDots: 'true',
     autoplay: 'true',
     circular: 'true',
     indicatorActiveColor: "#FF9C35",
     interval: 5000,
-    duration: 1000
+    duration: 1000,
   },
 
   /**
@@ -66,6 +96,16 @@ Page({
     wx.navigateTo({
       url: '../search/search'
     })
+  },
+  //添加菜品
+  add: function(e){
+    var that = this;
+
+  },
+  //减少菜品
+  reduce: function(e){
+    var that = this;
+    
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
